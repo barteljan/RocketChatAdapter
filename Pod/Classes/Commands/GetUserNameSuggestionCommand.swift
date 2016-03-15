3461d8f4630ead009d9dea317cc1da450471b418
Jan Bartel <barteljan@yahoo.de>
2016-03-15 20:41:54 +0100

    added set username, password mail, etc

---
+++ Pod/Classes/Commands/GetUserNameSuggestionCommand.swift
@@ -0,0 +1,17 @@
+//
+//  GetUserNameSuggestionCommand.swift
+//  Pods
+//
+//  Created by Jan Bartel on 15.03.16.
+//
+//
+
+import Foundation
+
+public protocol GetUserNameSuggestionCommandProtocol{
+
+}
+
+public struct GetUserNameSuggestionCommand : GetUserNameSuggestionCommandProtocol{
+
+}
